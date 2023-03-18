9e70188928873b2b9a47101bed85d92224921df3
Darryl Pitt <darrylpitt43@gmail.com>
2023-03-18 10:25:22 -0400

    added skeleton js, cs, and html

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function Player(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = () => {
-    let result = `${title} by ${author}, ${pages} `;
-    if (read === false) {
-      return result + `pages, not read yet`;
-    } else if (read === true) {
-      return result + `pages, has been read`;
-    }
-  };
+// function Player(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.info = () => {
+//     let result = `${title} by ${author}, ${pages} `;
+//     if (read === false) {
+//       return result + `pages, not read yet`;
+//     } else if (read === true) {
+//       return result + `pages, has been read`;
+//     }
+//   };
+// }
+
+// const theHobbit = new Player("The Hobbit", "J.R.R Tolkien", 295, true);
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor
 }
 
-const theHobbit = new Player("The Hobbit", "J.R.R Tolkien", 295, true);
+function addBookToLibrary() {
+  // do stuff here
+}
