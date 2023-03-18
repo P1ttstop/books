8eb701ed1309c52d68b7040e04cb65ebd8bda1c5
Darryl Pitt <DPitt@S4T.local>
2023-03-18 10:15:35 -0400

    added start files

---
+++ script.js
@@ -0,0 +1,16 @@
+function Player(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    let result = `${title} by ${author}, ${pages} `;
+    if (read === false) {
+      return result + `pages, not read yet`;
+    } else if (read === true) {
+      return result + `pages, has been read`;
+    }
+  };
+}
+
+const theHobbit = new Player("The Hobbit", "J.R.R Tolkien", 295, true);
